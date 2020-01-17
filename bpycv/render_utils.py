42b8bc8a8627f2badd8b6532e39ad1cea2bfcbbe
Rafael Colares <rcolares@l4491.seek.int>
2020-01-17 15:32:14 +1100

    Debuging

--- bpycv/render_utils.py
+++ bpycv/render_utils.py
@@ -70,6 +70,8 @@ def render_data(render_image=True, render_annotation=True):
         png_path = path + ".png"
         with set_image_render(), withattr(render, "filepath", png_path):
             print("Render image using:", render.engine)
+            print(png_path)
+            print('************')
             bpy.ops.render.render(write_still=True)
         render_result["image"] = imread(png_path)
         os.remove(png_path)
